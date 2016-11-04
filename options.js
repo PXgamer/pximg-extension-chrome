5bae41d09f5c9f2f00e068350b3a9c8aaec46173
PXgamer <owzie123@gmail.com>
2016-11-04 14:44:46 +0000

    Initial commit

---
+++ options.js
@@ -0,0 +1,23 @@
+function save_options() {
+  var api_key = document.getElementById('api_key').value;
+  chrome.storage.sync.set({
+    api_key: api_key
+  }, function() {
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 1500);
+  });
+}
+
+function restore_options() {
+  chrome.storage.sync.get({
+    api_key: ''
+  }, function(items) {
+    document.getElementById('api_key').value = items.api_key;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
---
+++ pximg.js
@@ -0,0 +1,74 @@
+var api_key = '';
+var returnData = '';
+chrome.storage.sync.get({
+    api_key: ''
+  }, function(items) {
+    api_key = items.api_key;
+});
+console.info(api_key);
+
+chrome.contextMenus.create(
+	{
+		"title": 	"Upload Image to PXIMG",
+		"contexts": ["image"],
+        "onclick": 	sendData
+	}
+);
+
+function showData(info) { // FOR DEBUGGING ONLY
+	console.info(info.srcUrl);
+}
+
+function sendData(info) {
+	// Set URL
+	var url 		= "https://pximg.xyz/api/v2/images/url/";
+	var iURL 		= info.srcUrl;
+	
+	// Setup POST variables
+	var formData 	= new FormData();
+	formData.append('api_key', api_key);
+	formData.append('url', iURL);
+	
+	// Begin XHR POST request
+	var x = new XMLHttpRequest();
+	x.open('POST', url);
+	x.responseType = 'json';
+	x.onload = function() {
+		pximg(x.response);
+	};
+	x.send(formData);
+}
+
+function pximg(response)
+{
+	textR = response;
+	console.log(response);
+	if (response.Status) {
+		returnData = textR.Response.url;
+		console.info(returnData);
+		chrome.notifications.create(
+			"PXIMG",
+			{
+				type: "basic",
+				title: "PXIMG",
+				iconUrl: "icon_128x128.png",
+				message: "Your image has been uploaded.\nClick to Copy.",
+			}
+		);
+		ctc(returnData);
+	}
+	else {
+		alert(response.Response);
+	}
+}
+function ctc(text) {
+	var copyDiv = document.createElement('div');
+	copyDiv.contentEditable = true;
+	document.body.appendChild(copyDiv);
+	copyDiv.innerHTML = text;
+	copyDiv.unselectable = "off";
+	copyDiv.focus();
+	document.execCommand('SelectAll');
+	document.execCommand("Copy", false, null);
+	document.body.removeChild(copyDiv);
+}
